e133cd564c46cfe41947081359998bb4336747d1
alex-njugi <alexnjugi11@gmail.com>
2025-06-27 13:12:51 +0300

    Trigger redeploy to create tables

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///db.sqlite3")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_TOKEN_LOCATION = ['cookies']
-    JWT_COOKIE_SECURE = False  # Set to True in production with HTTPS
+    JWT_COOKIE_SECURE = False  
     JWT_COOKIE_CSRF_PROTECT = False
