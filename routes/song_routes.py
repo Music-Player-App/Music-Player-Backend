53861fe4b79bd1f7c9ad4c2b522ff1fbfc9f00c6
Alex  Njugi  Karanja <alexnjugi11@gmail.com>
2025-06-27 22:00:21 +0300

    Update song_routes.py

--- routes/song_routes.py
+++ routes/song_routes.py
@@ -1,8 +1,7 @@
 from flask import Blueprint
-from controllers.song_controller import add_song, get_all_songs, search_songs
+from controllers.song_controller import add_song, search_songs  
 
 song_bp = Blueprint("songs", __name__, url_prefix="/api")
 
 song_bp.route("/songs", methods=["POST"])(add_song)
-song_bp.route("/songs", methods=["GET"])(get_all_songs)
-song_bp.route("/search", methods=["GET"])(search_songs)
+song_bp.route("/songs", methods=["GET"])(search_songs)  
